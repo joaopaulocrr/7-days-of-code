7e41c4cba29c93ab382a3a9ef418c2fef656e204
João Paulo <paulocorrentinense@gmail.com>
2023-07-30 08:44:01 -0300

    incremento_parte1

--- desafio2/script.js
+++ desafio2/script.js
@@ -1,6 +1,23 @@
-let name = document.querySelector('#iname')
+let nome = document.getElementById('iname')
 let age = document.querySelector('#iage')
 let language = document.querySelector('#ilanguage')
-let body = document.querySelector('body')
+const body = document.querySelector('body')
+const button = document.querySelector('#botao')
 
-body.insertAdjacentHTML("beforeend", `<p>Olá ${name}, </p>`)
+button.addEventListener("click", () => {
+    const newParagrafo = document.createElement('p')
+    body.appendChild(newParagrafo)
+    newParagrafo.innerHTML += `Olá ${nome.value}, você tem ${age.value} anos e já está aprendendo ${language.value}!`
+    // nome.value = ''
+    // age.value = ''
+    // language.value = ''
+    proximaPergunta()
+})
+
+
+function proximaPergunta() {
+    const paragrafo2 = document.createElement('p')
+    body.appendChild(paragrafo2)
+    paragrafo2.innerHTML = `Você gosta de estudar ${language.value}?`
+   
+}
