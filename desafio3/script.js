d0ae629fac109ea79a25fed1344d25830ce791c4
João Paulo <paulocorrentinense@gmail.com>
2023-08-02 09:45:02 -0300

    import_export

--- desafio3/script.js
+++ desafio3/script.js
@@ -1,27 +1,29 @@
-const buttonSend = document.querySelector('#send')
 
+import { escolhaBackend } from "./backend.js";
+import { escolhaFrontend } from "./frontend.js";
+
+const buttonSend = document.querySelector('#send')
+const body = document.getElementById('body')
+const parabens = document.createElement('p')
+body.appendChild(parabens)
+parabens.innerHTML = ''
 
 buttonSend.addEventListener('click', () => {
     const back = document.querySelector('#backend')
     const front = document.querySelector('#frontend')
-    
-    if(back.checked){
-        console.log('Meus parabéns! Você escolheu o  BACKEND!')
-    }else if(front.checked){
-        console.log('Meus parabéns! Você escolheu o FRONTEND!')
-    }else{
-        console.log('Opção inválida. Tente novamente.')
+
+
+    if (back.checked) {
+        parabens.innerHTML = 'Meus parabéns! Você escolheu o  BACKEND!'
+        parabens.innerHTML += '<p>Qual tecnologia deseja aprender a seguir?</p>'
+        escolhaBackend()
+
+    } else if (front.checked) {
+        parabens.innerHTML = 'Meus parabéns! Você escolheu o FRONTEND!'
+        escolhaFrontend()
+    } else {
+        parabens.innerHTML = 'Opção inválida. Tente novamente.'
+
     }
+
 })
-
-
-
-
-
-
-// if(front.cheked){
-//     console.log('front')
-// }else{
-//     console.log('back')
-
-// }
